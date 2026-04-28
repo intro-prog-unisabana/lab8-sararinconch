2d7609f58f360f94aa4ddbad0b7075943c7ed78a
Sara Rincón <sararich@unisabana.edu.co>
2026-04-28 18:09:22 -0500

    Problema 2 - Parte 1.2

--- todo_manager.py
+++ todo_manager.py
@@ -17,7 +17,7 @@ def write_todo_file(file_path, tasks):
     with open(file_path, 'w') as file:
         for task in tasks:
             file.write(task + "\n")
-    raise NotImplementedError
+
 
 
 
